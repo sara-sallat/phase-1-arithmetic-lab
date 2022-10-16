2aa8983bf835499c87e16a4dc229006e0f68edcb
sara sallat <sallatsara@gmail.com>
2022-10-16 14:20:17 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+ let multiply;
+ let num1=2;
+ let num2=31;
+ let random;
+ multiply=num1*num2;
+ console.log(multiply);
+ random=Math.floor(Math.random() * 10 ) +1;
+ console.log(random);
+ let num3=44;
+ let num4=5;
+ let mod=num3%num4;
+ console.log(mod);
+ let max=Math.max(11, 6, 20, 9 ,13, 4, 5)
+ console.log(max);
